176a17f563ce98a5d1f27831d179e58e944b72d4
Rubensramos32 <rubensramos955@gmail.com>
2024-09-05 19:19:10 -0300

    Update componentes.py
    Neste último commit, foram feitas alterações na interface gráfica do projeto para deixar mais agradável ao usuario.

--- componentes.py
+++ componentes.py
@@ -1,6 +1,6 @@
 import sqlite3
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, font
 import re
 
 # Conectar ao banco de dados SQLite
@@ -9,57 +9,57 @@ c = conn.cursor()
 
 # Criar tabela se não existir
 c.execute('''CREATE TABLE IF NOT EXISTS componentes (
-             codigo_barras TEXT PRIMARY KEY
+             codigo_componente TEXT PRIMARY KEY
              )''')
 conn.commit()
 
-# Função para validar o formato do código de barras
-def validar_codigo_barras(codigo_barras):
+# Função para validar o formato dos componentes
+def validar_codigo_componente(codigo_componente):
     padrao = re.compile(r'^\d{3}\.\d{4}\.\d{2}$')
-    return bool(padrao.match(codigo_barras))
+    return bool(padrao.match(codigo_componente))
 
 # Função para adicionar componente ao banco de dados
-def adicionar_componente(codigo_barras):
-    if not validar_codigo_barras(codigo_barras):
+def adicionar_componente(codigo_componente):
+    if not validar_codigo_componente(codigo_componente):
         messagebox.showerror("Erro", "Código de barras no formato inválido. Use o formato 000.0000.00")
         return
     
     try:
-        c.execute('INSERT INTO componentes (codigo_barras) VALUES (?)', (codigo_barras,))
+        c.execute('INSERT INTO componentes (codigo_componente) VALUES (?)', (codigo_componente,))
         conn.commit()
         messagebox.showinfo("Sucesso", "Componente adicionado ao seu estoque!")
         limpar_campos()
     except sqlite3.IntegrityError:
-        messagebox.showerror("Erro", "Componente com esse código de barras já existe no estoque.")
+        messagebox.showerror("Erro", "Componente com esse código já existe no estoque.")
 
 # Função para pesquisar componente pelo código de barras
 def pesquisar_componente():
-    codigo_barras = entry_pesquisa_codigo.get()
+    codigo_componente = entry_pesquisa_codigo.get()
 
-    if not validar_codigo_barras(codigo_barras):
-        messagebox.showerror("Erro", "Código de barras no formato inválido. Use o formato 000.0000.00")
+    if not validar_codigo_componente(codigo_componente):
+        messagebox.showerror("Erro", "Código do componente no formato inválido. Use o formato 000.0000.00")
         return
 
-    c.execute('SELECT * FROM componentes WHERE codigo_barras = ?', (codigo_barras,))
+    c.execute('SELECT * FROM componentes WHERE codigo_componente = ?', (codigo_componente,))
     resultado = c.fetchone()
     if resultado:
-        entry_codigo_barras.delete(0, tk.END)
-        entry_codigo_barras.insert(0, resultado[0])  # código de barras
+        entry_codigo_componente.delete(0, tk.END)
+        entry_codigo_componente.insert(0, resultado[0])  # código de barras
         messagebox.showinfo("Sucesso", "Componente encontrado.")
     else:
         messagebox.showinfo("Erro", "Componente não encontrado.")
         limpar_campos()
 
-# Função para remover componente pelo código de barras com confirmação
+# Função para remover componente pelo código com confirmação
 def remover_componente():
-    codigo_barras = entry_pesquisa_codigo.get()
+    codigo_componente = entry_pesquisa_codigo.get()
 
-    if not validar_codigo_barras(codigo_barras):
-        messagebox.showerror("Erro", "Código de barras no formato inválido. Use o formato 000.0000.00")
+    if not validar_codigo_componente(codigo_componente):
+        messagebox.showerror("Erro", "Código do componente no formato inválido. Use o formato 000.0000.00")
         return
     
     if messagebox.askyesno("Confirmação", "Você tem certeza que deseja remover este componente?"):
-        c.execute('DELETE FROM componentes WHERE codigo_barras = ?', (codigo_barras,))
+        c.execute('DELETE FROM componentes WHERE codigo_componente = ?', (codigo_componente,))
         if c.rowcount > 0:
             conn.commit()
             messagebox.showinfo("Sucesso", "Componente removido do estoque.")
@@ -69,12 +69,12 @@ def remover_componente():
 
 # Função para limpar campos de entrada
 def limpar_campos():
-    entry_codigo_barras.delete(0, tk.END)
+    entry_codigo_componente.delete(0, tk.END)
 
 # Função para lidar com a leitura do código de barras
-def ler_codigo_barras(event):
-    codigo_barras = entry_codigo_barras.get()
-    adicionar_componente(codigo_barras)
+def ler_codigo_componente(event):
+    codigo_componente = entry_codigo_componente.get()
+    adicionar_componente(codigo_componente)
 
 # Função para fechar a conexão com o banco de dados ao sair da aplicação
 def on_closing():
@@ -93,30 +93,36 @@ def abrir_gerenciador_componentes():
 
     root = tk.Tk()
     root.title(f"Gerenciador de Componentes Pessoais de {usuario}")
-    root.geometry("600x200")
+    root.geometry("600x300")
+    root.configure(bg="#363636")
+
+    # fontes 
+    titulo_font = font.Font(family="Helvetica", size=16, weight="bold")
+    texto_font = font.Font(family="Helvetica", size=12)
 
     # Widgets da janela principal
-    tk.Label(root, text="Pesquisar por código do componente:").pack()
+    tk.Label(root, text="Pesquisar por código do componente:", bg="#363636", fg="#ecf0f1", font=texto_font).pack(pady=10)
     global entry_pesquisa_codigo
-    entry_pesquisa_codigo = tk.Entry(root)
+    entry_pesquisa_codigo = tk.Entry(root, font=texto_font)
     entry_pesquisa_codigo.pack()
+    entry_pesquisa_codigo.bind('<Return>', lambda event: pesquisar_componente())
 
-    btn_pesquisar = tk.Button(root, text="Pesquisar", command=pesquisar_componente)
-    btn_pesquisar.pack()
+    btn_pesquisar = tk.Button(root, text="Pesquisar", command=pesquisar_componente, bg="#808080", fg="#ecf0f1", font=texto_font)
+    btn_pesquisar.pack(pady=5)
 
-    btn_remover = tk.Button(root, text="Remover Componente", command=remover_componente)
-    btn_remover.pack()
+    btn_remover = tk.Button(root, text="Remover Componente", command=remover_componente, bg="#B22222", fg="#ecf0f1", font=texto_font)
+    btn_remover.pack(pady=5)
 
-    tk.Label(root, text="\nAdicionar um novo componente ao seu estoque:").pack()
-    global entry_codigo_barras
-    entry_codigo_barras = tk.Entry(root)
-    entry_codigo_barras.pack()
-    entry_codigo_barras.bind('<Return>', ler_codigo_barras)  # Lê o código de barras quando Enter é pressionado
+    tk.Label(root, text="\nAdicionar um novo componente ao seu estoque:", bg="#363636", fg="#ecf0f1", font=texto_font).pack(pady=10)
+    global entry_codigo_componente
+    entry_codigo_componente = tk.Entry(root, font=texto_font)
+    entry_codigo_componente.pack()
+    entry_codigo_componente.bind('<Return>', ler_codigo_componente)  # Lê o código do componente quando Enter é pressionado
 
-    btn_adicionar = tk.Button(root, text="Adicionar Componente", command=lambda: adicionar_componente(entry_codigo_barras.get()))
-    btn_adicionar.pack()
+    btn_adicionar = tk.Button(root, text="Adicionar Componente", command=lambda: adicionar_componente(entry_codigo_componente.get()), bg="#008000", fg="#ecf0f1", font=texto_font)
+    btn_adicionar.pack(pady=5)
 
-    # Fechar a aplicação corretamente
+    # Fechar a aplicação corretamente para salvar informações
     root.protocol("WM_DELETE_WINDOW", on_closing)
 
     root.mainloop()
@@ -128,14 +134,17 @@ def login_enter(event):
 # Janela de login
 login_window = tk.Tk()
 login_window.title("Login")
+login_window.geometry("300x150")
+login_window.configure(background="#363636")
 
-tk.Label(login_window, text="Digite seu nome para entrar:").pack()
-entry_usuario = tk.Entry(login_window)
+tk.Label(login_window, text="Digite seu nome para entrar:", background="#363636", fg="#ecf0f1", font=("Helvetica", 12)).pack(pady=20)
+entry_usuario = tk.Entry(login_window, font=("Helvetica", 12))
 entry_usuario.pack()
+
 entry_usuario.bind('<Return>', login_enter)  # Lê o nome do usuário quando Enter é pressionado
 
-btn_entrar = tk.Button(login_window, text="Entrar", command=abrir_gerenciador_componentes)
-btn_entrar.pack()
+btn_entrar = tk.Button(login_window, text="Entrar", command=abrir_gerenciador_componentes, bg="#808080", fg="#ecf0f1", font=("Helvetica", 12))
+btn_entrar.pack(pady=10)
 
 login_window.mainloop()
 
